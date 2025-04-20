04a4400caec11a38530cae4510b86e18ceb13f9a
bulkmilk <bulkmilk@qq.com>
2025-04-21 07:00:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,10 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #sed -i '2i src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 #sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '4i src-git small https://github.com/kenzok8/small' feeds.conf.default
