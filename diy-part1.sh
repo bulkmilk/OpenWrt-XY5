3fa6332f52a7467aa3d47d892a8dfcbd8f4af874
bulkmilk <bulkmilk@qq.com>
2025-04-22 07:22:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,9 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 添加 ShadowSocksR Plus+ 插件
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-# Add a feed source
-#sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-#sed -i '2i src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
-#sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '4i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# 添加 PassWall 插件
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
